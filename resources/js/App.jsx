7421f6cba33ad84e758307d0c440ee818b81ad15
baadillahnabil <nabilzero27@gmail.com>
2019-01-18 17:02:10 +0700

    first commit. setup auth. scaffold react as frontend.

---
+++ resources/js/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        console.log('coook')
+        return (
+            <>
+                <p>Hello There !</p>
+            </>
+        )
+    }
+}
+
+export default App
