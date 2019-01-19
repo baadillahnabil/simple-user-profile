ee415bf264a521cc3bff32d94f6a03c6df4cf580
baadillahnabil <nabilzero27@gmail.com>
2019-01-19 21:05:27 +0700

    add handle to switch between component

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -4,9 +4,26 @@ import Logo from '../images/logo.png'
 import './App.module.scss'
 
 import Login from './Login/Login'
+import Register from './Register/Register'
 
 class App extends Component {
+  state = {
+    showing: 'login'
+  }
+
+  moveComponent = to => this.setState({ showing: to })
+
   render() {
+    const { showing } = this.state
+
+    let showedComponent
+    if (showing === 'login')
+      showedComponent = <Login handleMoveToRegister={this.moveComponent.bind(this, 'register')} />
+    else if (showing === 'register')
+      showedComponent = <Register handleMoveToLogin={this.moveComponent.bind(this, 'login')} />
+    // else if (showing === 'dashboard')
+    //   showedComponent = <Login handleMoveToLogin={this.moveComponent.bind(this, 'login')} />
+
     return (
       <>
         <div className="BoxedLayout">
@@ -14,7 +31,7 @@ class App extends Component {
             <Row className="CenteredRow">
               <Card body className="CardBody">
                 <Image src={Logo} className="ImageLogo" thumbnail roundedCircle />
-                <Login />
+                {showedComponent}
               </Card>
             </Row>
           </Container>
