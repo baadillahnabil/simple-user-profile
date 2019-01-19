4a9bc2f957ae3f60214671d64d68b6e855cff828
baadillahnabil <nabilzero27@gmail.com>
2019-01-20 03:02:25 +0700

    add controller to handle auth login and register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->request->add([
+            'grant_type' => 'password',
+            'client_id' => config('services.passport.client_id'),
+            'client_secret' => config('services.passport.client_secret'),
+            'username' => $request->username,
+            'password' => $request->password
+        ]);
+        $tokenRequest = Request::create(config('services.passport.login_endpoint'), 'post');
+        $response = Route::dispatch($tokenRequest);
+
+        return $response;
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'phone' => ['required', 'string', 'unique:users'],
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'regex:/^(?=.*?[a-z])(?=.*?[0-9])[a-zA-Z][a-zA-Z0-9]{7,}$/',
+                'confirmed'
+            ]
+        ]);
+
+        return User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'password' => Hash::make($request->password),
+        ]);
+    }
+}
