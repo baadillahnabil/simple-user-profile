6fc12f710c5151063835e307d0063844fb10110b
baadillahnabil <nabilzero27@gmail.com>
2019-01-19 21:07:31 +0700

    change title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Gigel.id Test</title>
+        <title>Simple User Profile</title>
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
