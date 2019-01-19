08c01e748276ac241f12d36e8dd9673a45ff3dfa
baadillahnabil <nabilzero27@gmail.com>
2019-01-19 20:04:12 +0700

    include login page

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
+import { Container, Row, Image, Card } from 'react-bootstrap'
+import Logo from '../images/logo.png'
+import './App.module.scss'
+
+import Login from './Login/Login'
 
 class App extends Component {
-    render() {
-        console.log('coook')
-        return (
-            <>
-                <p>Hello There !</p>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <div className="BoxedLayout">
+          <Container>
+            <Row className="CenteredRow">
+              <Card body className="CardBody">
+                <Image src={Logo} className="ImageLogo" thumbnail roundedCircle />
+                <Login />
+              </Card>
+            </Row>
+          </Container>
+        </div>
+      </>
+    )
+  }
 }
 
 export default App
