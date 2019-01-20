2cf210359d78811887a43ad9771d1fb059b60f80
baadillahnabil <nabilzero27@gmail.com>
2019-01-20 17:59:52 +0700

    optimizing code

--- app/User.php
+++ app/User.php
@@ -9,8 +9,7 @@ use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
-    use HasApiTokens;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
